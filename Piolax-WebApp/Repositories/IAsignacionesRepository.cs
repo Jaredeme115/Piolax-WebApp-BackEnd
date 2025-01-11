3488649d991b3f17d75b552f3aa50d88213abd84
SERGIO JARED RODRIGUEZ CALVO <sergio.rodriguezco@uanl.edu.mx>
2025-01-10 20:28:28 -0600

    Cambios Casa - Preeliminares

---
+++ Piolax-WebApp/Repositories/IAsignacionesRepository.cs
@@ -0,0 +1,16 @@
+﻿using Piolax_WebApp.DTOs;
+using Piolax_WebApp.Models;
+
+namespace Piolax_WebApp.Repositories
+{
+    public interface IAsignacionesRepository
+    {
+        Task<Asignaciones> RegistrarAsignacion(Asignaciones asignaciones);
+        Task<Asignaciones> ObtenerAsignacionConDetalles(int idAsignacion);
+        //Task<IEnumerable<Asignaciones>> ObtenerTodasLasAsignaciones();
+        //Task<IEnumerable<Asignaciones>> ObtenerAsignacionesPorTecnico(string numNomina);
+
+        //Método para obtener modificar el estatus de aprobación de la asignacion
+        //Task<SolicitudesDetalleDTO> ModificarEstatusAprobacionTecnico(int idAsignacion, int idStatusAprobacionTecnico);
+    }
+}
---
+++ Piolax-WebApp/Repositories/Impl/AsignacionesRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Piolax_WebApp.Models;
+
+namespace Piolax_WebApp.Repositories.Impl
+{
+    public class AsignacionesRepository(AppDbContext context): IAsignacionesRepository
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<Asignaciones> RegistrarAsignacion(Asignaciones asignaciones)
+        {
+            _context.Asignaciones.Add(asignaciones);
+            await _context.SaveChangesAsync();
+            return asignaciones;
+        }
+
+        public async Task<Asignaciones> ObtenerAsignacionConDetalles(int idAsignacion)
+        {
+            return await _context.Asignaciones
+                .Include(a => a.idSolicitud)
+                .Include(a => a.idEmpleado)
+                .Include(a => a.horaInicio)
+                .Include(a => a.horaTermino)
+                .Include(a => a.solucion)
+                .Include(a => a.idRefaccion)
+                .Include(a => a.cantidad)
+                .Include(a => a.maquinaDetenida)
+                .Include(a => a.CategoriaAsignacion)
+                .Include(a => a.StatusAprobacionTecnico)
+                .FirstOrDefaultAsync(a => a.idAsignacion == idAsignacion);
+        }
+
+        public async Task<IEnumerable<Asignaciones>> ObtenerTodasLasAsignaciones()
+        {
+            return await _context.Asignaciones
+                .Include(a => a.Solicitud)
+                .Include(a => a.Empleado)
+                .Include(a => a.Inventario)
+                .Include(a => a.CategoriaAsignacion)
+                .Include(a => a.StatusAprobacionTecnico)
+                .Include(a => a.Asignacion_Refacciones)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Asignaciones>> ObtenerAsignacionesPorTecnico(string numNomina)
+        {
+            return await _context.Asignaciones
+                .Include(a => a.Solicitud)
+                .Include(a => a.Empleado)
+                .Include(a => a.Inventario)
+                .Include(a => a.CategoriaAsignacion)
+                .Include(a => a.StatusAprobacionTecnico)
+                .Include(a => a.Asignacion_Refacciones)
+                .Where(a => a.Empleado.numNomina == numNomina)
+                .ToListAsync();
+        }
+
+    
+    }
+}
